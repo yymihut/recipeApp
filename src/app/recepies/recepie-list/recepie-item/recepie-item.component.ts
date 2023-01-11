80b728802374d1e31c494212476d70959260afc2
Mihut Marius <yymihut@gmail.com>
2023-01-11 22:57:03 +0200

    fbfbgf

--- src/app/recepies/recepie-list/recepie-item/recepie-item.component.ts
+++ src/app/recepies/recepie-list/recepie-item/recepie-item.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component , Input, OnInit} from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recepie-item',
   templateUrl: './recepie-item.component.html',
   styleUrls: ['./recepie-item.component.css']
 })
-export class RecepieItemComponent {
+export class RecepieItemComponent implements OnInit {
+  @Input() reteta: Recipe;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
